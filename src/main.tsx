7c86c95729c67d09e2a0211f2cb511acfadc7572
19984646 <busetaktas45@gmail.com>
2024-12-30 23:53:34 +0330

    latest

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,8 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 
 
-WebApp.ready();        WebApp.expand();
+    
+  WebApp.expand();
 
 
 
