2c3471975f7bdae0ea3a85c8587ea6ab207657a9
19984646 <busetaktas45@gmail.com>
2025-01-20 10:56:13 +0330

    ads

--- src/pages/LocalStorageViewer.tsx
+++ src/pages/LocalStorageViewer.tsx
@@ -152,14 +152,14 @@ const AccountEquityCard: React.FC = () => {
     // BackButton'u görünür yap ve tıklanma işlevi ekle
     backButton.show();
     backButton.onClick(() => {
-      navigate("/latest-booba/top");
+      navigate("/latest-booba/");
     });
 
     // Cleanup: Bileşen unmount olduğunda butonu gizle ve event handler'ı kaldır
     return () => {
       backButton.hide();
       backButton.offClick(() => {
-        navigate("/latest-booba/top"); // Buraya tekrar aynı callback sağlanmalıdır.
+        navigate("/latest-booba/"); // Buraya tekrar aynı callback sağlanmalıdır.
       });
     };
   }, [navigate]);
