5d48dc995dfac7d3a6fbfafb63506dae8f83fd13
19984646 <busetaktas45@gmail.com>
2025-01-25 12:50:32 +0330

    ads

--- src/pages/LocalStorageViewer.tsx
+++ src/pages/LocalStorageViewer.tsx
@@ -372,7 +372,7 @@ useEffect(() => {
     <ThemeProvider theme={theme}>
   
  
-              <Box mt={"8vh"}>
+              <Box mt={"10vh"}>
         {/* İlk Kart */}
         <Box sx={{ borderRadius: 3, mt: 1, mx: 1,p:2, backgroundColor: '#3f3f3f' }}>
             {/* Total Account Equity */}
