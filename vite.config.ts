b2fe5eeb394a4154ed1d7c180890335e16c7335c
19984646 <busetaktas45@gmail.com>
2025-02-10 23:13:30 +0330

    ads

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 export default defineConfig({
   plugins: [react(), nodePolyfills()],  
   
-  base: '/latest-booba/' ,
+  base: '/' ,
 });
