f032bcf84a2bc28fed28e5dff2fe07d5a5f95788
19984646 <busetaktas45@gmail.com>
2025-02-11 00:43:35 +0330

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,4 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],  
-  
-  base: '/latest-booba/' ,
 });
