7b8ec62b7c0fb74bfa1d00a988996e08974c674b
19984646 <busetaktas45@gmail.com>
2025-02-12 01:25:44 +0330

    hot update .env

--- src/pages/firebaseConfig.ts
+++ src/pages/firebaseConfig.ts
@@ -7,24 +7,14 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-  apiKey: "AIzaSyBQwjgfS2FZWZJtGJPFAJNS58YcAdUVDeE",
-
-  authDomain: "telegram-cc828.firebaseapp.com",
-
-  projectId: "telegram-cc828",
-
-  storageBucket: "telegram-cc828.appspot.com",
-
-  messagingSenderId: "541310609374",
-
-  appId: "1:541310609374:web:5c046692c1ee59ba981f4f",
-
-  measurementId: "G-7XGPVD7NY4"
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
