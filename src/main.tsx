c6b794b6755d77c7e13796742afa512316c52040
19984646 <busetaktas45@gmail.com>
2025-02-14 00:31:28 +0330

    anlaytics

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import "slick-carousel/slick/slick.css"; // Basic styles for the slider
 import "slick-carousel/slick/slick-theme.css"; // Theme styles for the slider
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LocalStorageViewer from "./pages/LocalStorageViewer.tsx";
-import { Analytics } from '@vercel/analytics/react';
 
 import DealsComponent from "./pages/Tasks.tsx";
 import TestComponent from "./pages/TestComponent.tsx";
@@ -74,7 +73,6 @@ const DefaultErrorElement = () => (
 // Performance monitoring function
 const reportWebVitals = (metric: any) => {
   if (metric.label === 'web-vital') {
-    // Analytics'e gönder veya console'a yazdır
     console.log(metric);
   }
 };
@@ -209,7 +207,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
-      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
