673112f4888f0f6ae0a4cc1db9ba512bb5652534
19984646 <busetaktas45@gmail.com>
2025-02-16 01:06:08 +0330

    control added

--- src/pages/TaskAdsKey.tsx
+++ src/pages/TaskAdsKey.tsx
@@ -4,10 +4,10 @@ import { Box, Drawer, Typography, Button } from "@mui/material";
 import { getFirestore, doc, updateDoc, increment } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './firebaseConfig';
+import DirectLinkKeys from "../components/Ads/DirectLinkKeys";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 
 /**
   * Check Typescript section
@@ -23,9 +23,7 @@ export const Task = ({ debug, blockId }: TaskProps) => {
   const taskRef = useRef<AdsgramTaskElement>(null);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-
-
- 
+  const [isTaskAvailable, setIsTaskAvailable] = useState(true);
 
   const handleClaim = async () => {
     try {
@@ -47,18 +45,25 @@ export const Task = ({ debug, blockId }: TaskProps) => {
   };
 
   useEffect(() => {
-    const handler = (_event: CustomEvent<string>) => {
+    const rewardHandler = (_event: CustomEvent<string>) => {
       setIsDrawerOpen(true);
     };
+
+    const bannerNotFoundHandler = () => {
+      setIsTaskAvailable(false);
+    };
+
     const task = taskRef.current;
 
     if (task) {
-      task.addEventListener("reward", handler);
+      task.addEventListener("reward", rewardHandler);
+      task.addEventListener("onBannerNotFound", bannerNotFoundHandler);
     }
 
     return () => {
       if (task) {
-        task.removeEventListener("reward", handler);
+        task.removeEventListener("reward", rewardHandler);
+        task.removeEventListener("onBannerNotFound", bannerNotFoundHandler);
       }
     };
   }, []);
@@ -67,9 +72,12 @@ export const Task = ({ debug, blockId }: TaskProps) => {
     return null;
   }
 
+  if (!isTaskAvailable) {
+    return <DirectLinkKeys />;
+  }
+
   return (
     <>
-     
         <>
           <adsgram-task
             className={styles.task}
@@ -118,7 +126,7 @@ export const Task = ({ debug, blockId }: TaskProps) => {
                   mb: 3
                 }}
               >
-                Now you can craft with 5 key parts !
+                Now you can craft a KEY with 5 key parts !
               </Typography>
               <Button
                 variant="contained"
@@ -138,7 +146,6 @@ export const Task = ({ debug, blockId }: TaskProps) => {
             </Box>
           </Drawer>
         </>
-     
     </>
   );
 };
